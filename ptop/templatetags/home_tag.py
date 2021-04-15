83815c8ec969f4041b170569120862a44148e5d6
Hikaru Souda <souda@med.id.yamagata-u.ac.jp>
2021-04-15 20:37:27 +0900

    ver20210415c

--- ptop/templatetags/home_tag.py
+++ ptop/templatetags/home_tag.py
@@ -30,3 +30,11 @@ def percent(value, args):
     else:
         return str(round(value*100.0, args))+'%'
 #        return round(value*100.0, args)
+
+@register.simple_tag
+def url_replace(request, field, value):
+    """GETパラメータを一部を置き換える"""
+
+    url_dict = request.GET.copy()
+    url_dict[field] = str(value)  # Django2.1の一部対策。通常はvalueだけでOK
+    return url_dict.urlencode()
