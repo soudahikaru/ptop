2fa825b1b6909db56d5c8ddd8220e7b568cea1b8
Hikaru Souda <souda@med.id.yamagata-u.ac.jp>
2022-05-20 18:51:05 +0900

    ver20220520a

--- ptop/models.py
+++ ptop/models.py
@@ -139,10 +139,6 @@ class SiteDetail(models.Model):
         return self.title
 
 
-def create_default_site_detail(sender, **kwargs):
-    site = Site.objects.get(pk=settings.SITE_ID)
-    SiteDetail.objects.get_or_create(site=site)
-
 #   メールの送信に関するメソッド
 #   def email_user(self, subject, message, from_email=None, **kwargs):
 #       send_mail(subject, message, from_email, [self.email], **kwargs)
